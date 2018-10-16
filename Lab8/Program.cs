8e9cab975484803af76d8581d76a33b9d8d215bb
Jordan Owiesny <jordanowiesny@quickenloans.com>
2018-10-16 16:46:08 -0400

    Tided up some code

--- Lab8/Program.cs
+++ Lab8/Program.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-
-
 /*
  *Messy im not sure it needs to be, but wanted to emphasis 2d array table
  */
@@ -34,6 +31,10 @@ namespace Lab8
         {
             int num = 0;
             string word = Console.ReadLine();
+            if (word == "Town" || word == "1")
+                return 0;
+            else if (word == "Food" || word == "2")
+                return 1;
             if (int.TryParse(word,out num))
             {
                 return num-1;
@@ -74,27 +75,13 @@ namespace Lab8
 
         static void Main(string[] args)
         {
-            string[,] students = new string[15, 3] {
-            {"Michael", "Canton", "Chicken Wings" },
-            { "Taylor", "Caro", "Cordon Bleu" },
- { "Joshua", "Taylor", "Turkey" },
- { "Lin-Z", "Toledo", "Ice Cream" },
- { "Madelyn", "Oxford", "Croissent" },
- { "Nana", "Guana", "Empanadas"},
- { "Rochelle", "Mars", "Space Cheese"},
- { "Shah", "Newark", "Chicken Wings"},
-  { "Tim", "Detroit", "Chicken Parm"},
-   { "Abby", "Traverse City", "Soup"},
-   { "Blake", "Los Angeles", "Cannolis"},
-   { "Bob", "St. Clair Shores", "Pizza"},
-   { "Jordan", "Warren", "Burgers"},
-   { "Jay", "Macomb", "Pickles"},
-   { "Jon", "Huntington Woods", "Ribs"},};
+            string[,] students = new string[15, 3] {{"Michael", "Canton", "Chicken Wings" },{ "Taylor", "Caro", "Cordon Bleu" },{ "Joshua", "Taylor", "Turkey" },
+            { "Lin-Z", "Toledo", "Ice Cream" },{ "Madelyn", "Oxford", "Croissent" },{ "Nana", "Guana", "Empanadas"},{ "Rochelle", "Mars", "Space Cheese"},
+            { "Shah", "Newark", "Chicken Wings"},{ "Tim", "Detroit", "Chicken Parm"},{ "Abby", "Traverse City", "Soup"},{ "Blake", "Los Angeles", "Cannolis"},
+            { "Bob", "St. Clair Shores", "Pizza"},{ "Jordan", "Warren", "Burgers"},{ "Jay", "Macomb", "Pickles"},{ "Jon", "Huntington Woods", "Ribs"},};
             bool[,] checker = new bool[students.GetLength(0), students.GetLength(1)];
             string temp = "";
             int studentNumber, category = 0;
-
-
             while (true)
             {
                 try
@@ -103,11 +90,7 @@ namespace Lab8
                     Console.Write("Enter student name OR 1-15: ");
                     studentNumber = FINDER(students);
                     Console.Write("Town or 1 | Food or 2: ");
-                    temp = Console.ReadLine();
-                    if (temp == "Town" || temp == "1")
-                        category = 1;
-                    else if (temp == "Food" || temp == "2")
-                        category = 2;
+                    category = FINDER(students)+1;
                     checker[studentNumber, category] = true;
                 }
                 catch(Exception ex)

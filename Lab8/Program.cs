bedc2ec2afda242af03a8abf100c8a1d63553fe4
Jordan Owiesny <jordanowiesny@quickenloans.com>
2018-10-16 13:11:32 -0400

    added expcetion and a function

--- Lab8/Program.cs
+++ Lab8/Program.cs
@@ -8,6 +8,31 @@ namespace Lab8
 {
     class Program
     {
+        static void available(bool[,] checker)
+        {
+            string s = "";
+            bool and = false;
+            for (int i = 0; i < checker.GetLength(0); i++)
+                if (checker[i, 0] == false)
+                {
+                    s += "Category 1 is still available";
+                    and = true;
+                    break;
+                }
+            for (int i = 0; i < checker.GetLength(0); i++)
+                if (checker[i, 1] == false && and == true)
+                {
+                    s += ", and category 2 is still available";
+                    break;
+                }
+                else if (checker[i,1] == false)
+                {
+                    s = "Category 2 is still available";
+                    break;
+                }
+            Console.WriteLine(s);
+        }
+
         static void Main(string[] args)
         {//I got a little fancy for no reason
             string[,] students = new string[2, 2] { {"Warren","Burger" },{"Gana","Egg" } };
@@ -19,17 +44,19 @@ namespace Lab8
 
             while (true)
             {
-                ask1:
-                Console.Write("\nChoose a student (theres 2): ");
-                studentNumber = int.Parse(Console.ReadLine()) - 1;
-                Console.Write("Enter 1 for town, 2 for food: ");
-                category = int.Parse(Console.ReadLine()) - 1;
-                if (studentNumber >= students.GetLength(0) || category >= students.GetLength(1))
+                try
                 {
-                    Console.WriteLine("One or more entries was too big...");
-                    goto ask1;
+                    Console.Write("\nChoose a student (theres 2): ");
+                    studentNumber = int.Parse(Console.ReadLine()) - 1;
+                    Console.Write("Enter 1 for town, 2 for food: ");
+                    category = int.Parse(Console.ReadLine()) - 1;
+                    checker[studentNumber, category] = true;
                 }
-                checker[studentNumber, category] = true;
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
                 Console.WriteLine("Name:".PadRight(10) + "Town:".PadRight(10) + "Food:".PadRight(10));
                 Console.WriteLine("============================");
                 for (int i = 0; i < students.GetLength(0); i++)
@@ -42,6 +69,7 @@ namespace Lab8
                     }
                     Console.WriteLine();
                 }
+                available(checker);
                 ask2:
                 Console.Write("Again(y/n): ");
                 temp = Console.ReadLine().ToLower();
